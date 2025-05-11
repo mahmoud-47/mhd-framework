e265f463db85e5dc94b58107709a92811587d8e6
mahmoud-47 <gysoft.sn@gmail.com>
2025-05-11 04:27:31 +0200

    move to wsl

---
+++ build/CMakeFiles/my_server.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_server.dir/router/routes.cpp.o"
+  "CMakeFiles/my_server.dir/server.cpp.o"
+  "CMakeFiles/my_server.dir/utils/request/Request.cpp.o"
+  "my_server"
+  "my_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
