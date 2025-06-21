6b8794de8af002b4cc63c8ffbdf93e38e2a368d9
Mohamed Gaye <gysoft.sn@gmail.com>
2025-06-21 16:09:11 +0200

    Modified user constructors and added SHA256 implementation

--- Models/user.hpp
+++ Models/user.hpp
@@ -6,7 +6,8 @@
 class User : public SQLiteORM {
 public:
     int id;
-    std::string name;
+    std::string firstname;
+    std::string lastname;
     std::string email;
     std::string username;
     std::string password;
@@ -15,8 +16,9 @@ public:
     void registerFields(){
         fields.clear();
         fields.push_back({"id", "INTEGER", &id, "PRIMARY KEY"});
-        fields.push_back({"name", "VARCHAR(50)", &name, ""});
-        fields.push_back({"email", "VARCHAR(100)", &email, "UNIQUE"});
+        fields.push_back({"firstname", "VARCHAR(50)", &firstname, ""});
+        fields.push_back({"lastname", "VARCHAR(20)", &lastname, ""});
+        fields.push_back({"email", "VARCHAR(100)", &email, ""});
         fields.push_back({"username", "VARCHAR(100)", &username, "UNIQUE"});
         fields.push_back({"password", "VARCHAR(255)", &password});
     }
@@ -26,16 +28,26 @@ public:
     }
 
     // Proper copy constructor
-    User(const User& other) : SQLiteORM(other), 
-                            id(other.id),
-                            name(other.name),
-                            email(other.email) {
-        registerFields();  // This updates the pointers to our new members
+    User(const User& other) : SQLiteORM(other),
+        id(other.id),
+        firstname(other.firstname),
+        lastname(other.lastname),
+        email(other.email),
+        username(other.username),
+        password(other.password) {
+        registerFields();
     }
 
     // Parameterized constructor
-    User(const int id, const std::string& name, const std::string& email) : 
-        SQLiteORM("users"), id(id), name(name), email(email) {
+    User(int id, const std::string& firstname, const std::string& lastname, const std::string& email, const std::string& username, const std::string& password)
+        : SQLiteORM("users"),
+        id(id),
+        firstname(firstname),
+        lastname(lastname),
+        email(email),
+        username(username),
+        password(password) 
+    {
         registerFields();
     }
 
@@ -44,6 +56,21 @@ public:
 
     ORMModel* clone() const override {
         return new User(*this);
+    }
+
+
+    /**
+     * User Authentication methods
+     */
+    // Returns the user id after successfuly created the user, or returns -1
+    static int create_user(
+        const std::string &firstname, 
+        const std::string &lastname, 
+        const std::string &email, 
+        const std::string &username, 
+        const std::string &password
+    ){
+        
     }
 
 };
